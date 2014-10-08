ec0f8c5f8fe8b037da2bd3e3c07be2451d4b3cc7
rlyshw <rlyshw@gmail.com>
2014-10-08 09:50:01 -0400

    create vcard for user in CreateUser

--- app/src/main/java/com/example/rlysh_000/xmppsms/CreateUser.java
+++ app/src/main/java/com/example/rlysh_000/xmppsms/CreateUser.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import org.jivesoftware.smack.AccountManager;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smackx.vcardtemp.packet.VCard;
 
 /**
  * Created by rlysh_000 on 9/25/2014.
@@ -26,7 +27,11 @@ public class CreateUser extends AsyncTask<String, Integer, Boolean>{
     protected Boolean doInBackground(String... strings) {
         try {
             // Try to create an account on the server
+            // Strings[0] is mPhoneNumber strings[1] is password (not necessary?)
             accountManager.createAccount(strings[0], strings[1]);
+            VCard vcard = new VCard();
+            vcard.setNickName(strings[0]);
+            vcard.save(XmppTask.connection);
             return true;
         } catch (XMPPException e1) {
             Log.d(e1.getMessage(), e1.toString());
